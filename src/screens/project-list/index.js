a1f1f5816166b0f806bbbe15fed483fdc1b19de7
ALiuHua <liuhua6606@gmail.com>
2022-10-25 22:05:36 +1100

    feat: add useMount and useDebounce hooks

--- src/screens/project-list/index.js
+++ src/screens/project-list/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 import * as qs from "qs";
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
@@ -14,24 +14,29 @@ export const ProjectListScreen = () => {
   const [param, setParam] = useState({ name: "", personId: "" });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
+    console.log("run");
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
       }
-    );
-  }, [param]);
-  useEffect(() => {
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    console.log("running");
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
   return (
     <div>
+      {console.log("div")}
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
     </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 //在一个函数中，改变传入的对象本身是不好的，因为js里的对象是引用类型；会造成传入的对象受到污染
@@ -13,3 +15,40 @@ export const cleanObject = (obj) => {
   });
   return result;
 };
+
+//处理只在组件第一次加载时调用useEffect
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+//useEffect是个天然闭包
+//状态 state是干什么的呢？ 状态通常是响应式的，也就是它的改变我们应该能检测到它改变，页面可以跟着改变，或者useEffect跟着触发
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    console.log("debounce", debouncedValue);
+    //每次在value变化以后设置一个定时器
+    const timeOut = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    //component re-evaluated的时候会清理上次的数据
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [value, delay, debouncedValue]);
+  return debouncedValue;
+};
+
+/**
+ function test() {
+  let a = 1;
+  setTimeout(() => {
+    a = a + 50;
+  }, 2000);
+  return a;
+}
+const b = test();
+console.log(b);     //1
+ */
