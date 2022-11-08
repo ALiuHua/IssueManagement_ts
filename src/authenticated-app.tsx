16c65290eaaed0b34044966255ad3aa1c810ab41
ALiuHua <liuhua6606@gmail.com>
2022-11-08 15:29:50 +1100

    style:add Row styled component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,36 @@
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header gap={2}>
+        <h3>Logo</h3>
+        <h3>用户</h3>
+        <h3>项目</h3>
+        <button onClick={logout}>登出</button>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+const Container = styled.div`
+  /* display: grid; */
+  grid-template-columns: 6rem 1fr;
+  height: 100vh;
+`;
+const Header = styled(Row)`
+  height: 6rem;
+  background-color: gray;
+  & :last-child {
+    margin-left: auto;
+  }
+`;
+const Main = styled.main`
+  grid-area: main;
+`;
